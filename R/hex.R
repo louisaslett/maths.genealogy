7c4fb067aa37289e69d538990205d5b35be76068
Louis Aslett <louis.aslett@durham.ac.uk>
2024-11-15 00:45:18 +0000

    lintr fixes to hex logo generator

--- R/hex.R
+++ R/hex.R
@@ -1,17 +1,17 @@
 make_hex <- function() {
   imgurl <- system.file("maths_genealogy_img.png", package = "maths.genealogy")
   hexSticker::sticker(imgurl,
-                      s_x = 1,
+                      s_x = 1.0,
                       s_y = 1.025,
                       s_width = 0.85,
                       s_height = 0.85,
-                      package="",
+                      package = "",
                       p_x = 0.6,
                       p_y = 1.45,
                       p_color = "#FFFFFF",
                       p_family = "serif",
                       p_fontface = "bold",
-                      p_size = 5,
+                      p_size = 5.0,
                       h_fill = "#1F2528",
                       h_color = "#EDEDED",
                       url = "genealogy.louisaslett.com",
@@ -20,9 +20,9 @@ make_hex <- function() {
                       u_color = "#EDEDED",
                       u_x = 1.78,
                       u_y = 0.515,
-                      u_angle = 90,
+                      u_angle = 90.0,
                       white_around_sticker = TRUE,
-                      filename="inst/maths_genealogy_hex.png",
-                      dpi=600)
-  usethis::use_logo("inst/maths_genealogy_hex.png", geometry = "480x556")
+                      filename = file.path("inst", "maths_genealogy_hex.png"),
+                      dpi = 600L)
+  usethis::use_logo(file.path("inst", "maths_genealogy_hex.png"), geometry = "480x556")
 }
