20a2aad3aec6949fcbc2ba632154abf71e6852db
Louis Aslett <louis.aslett@durham.ac.uk>
2024-11-22 00:06:32 +0000

    Fix non-ASCII character and integer lint

--- R/plot_grviz.R
+++ R/plot_grviz.R
@@ -12,11 +12,11 @@ digraph {
 
 )"
 
-  dot <- paste0(dot, paste0("  ", names(graph[["nodes"]]), " [label=\"", gsub('"', '\\"', gsub("'", "’", graph[["nodes"]], fixed = TRUE), fixed = TRUE), "\"];\n", collapse = ""), "\n")
+  dot <- paste0(dot, paste0("  ", names(graph[["nodes"]]), " [label=\"", gsub('"', '\\"', gsub("'", "\u2019", graph[["nodes"]], fixed = TRUE), fixed = TRUE), "\"];\n", collapse = ""), "\n")
   dot <- paste0(dot, paste0("  ", graph[["edges"]][["from"]], " -> ", graph[["edges"]][["to"]], ";\n", collapse = ""))
   dot <- paste0(dot, "}\n")
 
   grv <- DiagrammeR::grViz(dot)
   svg <- DiagrammeRsvg::export_svg(grv)
-  svgPanZoom::svgPanZoom(svg, minZoom = 1.0, maxZoom = 20, width = "100%", height = "92vh")
+  svgPanZoom::svgPanZoom(svg, minZoom = 1.0, maxZoom = 20.0, width = "100%", height = "92vh")
 }
