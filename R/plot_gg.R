d596835b69991a884f67eb48108793a206b24fa2
Louis Aslett <louis.aslett@durham.ac.uk>
2024-11-30 12:28:16 +0000

    Improvements to ggenealogy plots for the kind of ancestries we are plotting

--- R/plot_gg.R
+++ R/plot_gg.R
@@ -26,6 +26,10 @@
 #'        Note that the ID must be one of the IDs searched when calling [get_genealogy()] to construct `g`, since the search for ancestors/descendants only goes directly up/down branches reachable from the initial search ID.
 #' @param col
 #'        a `character(1)` specifying the colour to highlight the mathematician one whom the graph is centred.
+#' @param expand
+#'        a `numeric(1)` with the expansion factor for the graph.
+#'        This defaults to `0.15`, with larger values causing the `x` axis to expand, smaller values for it to shrink.
+#'        This is useful if the nearest common ancestor has a long name, which may cause it to be clipped when plotting: increase this expansion factor to rectify this.
 #'
 #' @return
 #' An object of class `("gg", "ggplot")` which can be displayed, or further manipulated using additional layers or aesthetic modifications from the [`ggplot2`][ggplot2::ggplot2] package.
@@ -34,7 +38,7 @@
 #'
 #' @examples
 #' # TODO
-plot_gg <- function(g, max_anc = 3L, max_des = 3L, id = NULL, col = "red") {
+plot_gg <- function(g, max_anc = 3L, max_des = 3L, id = NULL, col = "red", expand = 0.15) {
   # Check inputs and for required packages
   check_genealogy(g)
 
@@ -70,5 +74,25 @@ plot_gg <- function(g, max_anc = 3L, max_des = 3L, id = NULL, col = "red") {
 
   pc <- get_parent_child(g, g[[as.character(id)]][["name"]], max_anc, max_des)
 
-  ggenealogy::plotAncDes(g[[as.character(id)]][["name"]], pc, mAnc = max_anc, mDes = max_des, vColor = col)
+  # lintr "no visible binding" fixes
+  x <- y <- xend <- yend <- NULL
+
+  # Construct plot with ggenealogy
+  original_plot <- ggenealogy::plotAncDes(g[[as.character(id)]][["name"]], pc, mAnc = max_anc, mDes = max_des, vColor = col)
+  # Now modify this to work better visually for these kinds of ancestries
+  # Want to make lines grey so the text is easier to read, so overplot original
+  # lines and then place original text layers back on top.
+  # In addition, these read better vertically, so flip coordinates and reverse
+  # axis so that older generations at the top
+  original_plot +
+    ggplot2::geom_segment(data = ggplot2::ggplot_build(original_plot)[["data"]][[2L]], # Fetch the segment data
+                          ggplot2::aes(x = x, y = y, xend = xend, yend = yend),
+                          colour = "grey", size = 1.2, inherit.aes = FALSE) +
+    ggplot2::geom_segment(data = ggplot2::ggplot_build(original_plot)[["data"]][[3L]], # Fetch the segment data
+                          ggplot2::aes(x = x, y = y, xend = xend, yend = yend),
+                          colour = "grey", size = 1.2, inherit.aes = FALSE) +
+    original_plot[["layers"]][-(2L:3L)] +
+    ggplot2::coord_flip() +
+    ggplot2::scale_x_reverse() +
+    ggplot2::scale_y_continuous(expand = ggplot2::expansion(expand))
 }
